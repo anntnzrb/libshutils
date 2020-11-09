5d4cb1cc6fa22f63f5de2ea2160fd1aa7db73fa8
anntnzrb <anntnzrb@protonmail.com>
2020-11-08 20:38:42 -0500

    formatting + spelling

--- lib/doc_parser.sh
+++ lib/doc_parser.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
 # Generate html documentation for a script
+#
 # Sample usage:
 #   doc_parser script.sh
 # To parse multiple files at once:
 #   printf "%s\n" ./* | xargs -I{} -n1 ./doc_parser.sh '{}'
 
+# FIXME: 'change echo -> printf for portability' [Sun, 08 Nov ]
 is_docs() {
     # **
-    # Checks is $1 is documentation or just decoration
+    # Checks is "$1" is documentation or just decoration
     # *
+
     if [ -n "$1" ] && [ "$(printf "%c" "$1")" != '*' ]; then
         echo 0
     else
         echo -1
     fi
+
+    # TODO: 'shorten if possible' [Sun, 08 Nov]
+    #[ -n "$1" ] && [ "$(printf "%c" "$1")" != '*' ] && echo 0 || echo -1
 }
 
 wrap() {
     # **
-    # Wraps $1 in the HTML tag $2
+    # Wraps "$1" in the HTML tag "$2"
     # *
+
     body="$1"
     tag="$2"
 
@@ -29,22 +36,25 @@ wrap() {
 
 wrap_p() {
     # **
-    # Wraps $1 in 'p' tags
+    # Wraps "$1" in 'p' tags
     # *
+
     wrap "$1" 'p'
 }
 
 wrap_h3() {
     # **
-    # Wraps $1 in 'h' tags
+    # Wraps "$1" in 'h' tags
     # *
+
     wrap "$1" 'h3'
 }
 
 trim() {
     # **
-    # Trims leading and trailing whitespace from $1
+    # Trims leading and trailing whitespace from "$1"
     # *
+
     echo "$1" | awk '{gsub(/^ +| +$/,"");print}'
 }
 
@@ -52,6 +62,7 @@ basename() {
     # **
     # Extracts the basename from $1
     # *
+
     echo "${1##*/}"
 }
 
@@ -69,6 +80,6 @@ while read -r x xs; do
             desc="${desc}${xs} "
         fi
     fi
-done < "${1:-/dev/stdin}"
+done <"${1:-/dev/stdin}"
 
 wrap_p "$(trim "$desc")"
